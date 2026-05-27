c435e1268596e77ceebcff667ce0e371dffda91d
adeebnawshad <adeeb.nawshad@mail.utoronto.ca>
2026-05-27 09:57:27 +0600

    Add: coin-change - submission-8

---
+++ Data Structures & Algorithms/coin-change/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c]) # c itself + dp[a - c] (can be multiple possible such as 1 + dp[5] or 3 + dp[3] or 4 + dp[2] for 7 so we need to take the minimum)
+        return dp[amount] if dp[amount] != amount + 1 else -1
