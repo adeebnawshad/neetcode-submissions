d1b385d506039147872c025afdf5c98dc90b78f8
adeebnawshad <adeeb.nawshad@mail.utoronto.ca>
2026-07-14 19:53:48 +0600

    Add: find-minimum-in-rotated-sorted-array - submission-11

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-11.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        # [3, 4, 5, 6, 1, 2]
+        # get out of the loop as soon as l == r as that is where the minimum will be
+        while l < r:
+            mid = (l + r) // 2
+            # minimum (pivot) must be on the right if the array is rotated
+            # if mid in left sorted array (nums[mid] > nums[r]), minimum (pivot) must be to the right of mid, so move left pointer to after mid
+            if nums[mid] > nums[r]: # if isValid(mid)
+                l = mid + 1
+            # if mid is in the right sorted array, minimum(pivot) can be anywhere at or before mid (as minimum is the first element in the right sorted array), so move the right pointer to mid
+            else:
+                r = mid
+        # minimum is at l when we get out of the loop. W
+        return nums[r]
+
+                
+            
