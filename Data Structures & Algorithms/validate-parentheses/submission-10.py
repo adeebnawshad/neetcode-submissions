fe699bf7b210fa45ba85cf3835b1e20c2edc149a
adeebnawshad <adeeb.nawshad@mail.utoronto.ca>
2026-07-16 13:46:27 +0600

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {')':'(', '}':'{', ']':'['}
+        for c in s:
+            if c in closeToOpen:
+                if not stack:
+                    return False
+                if stack.pop() != closeToOpen[c]: # last open bracket is the first opening bracket to close
+                    return False
+            else:
+                stack.append(c)
+        return False if stack else True
