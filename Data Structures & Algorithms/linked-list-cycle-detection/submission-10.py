df6636e01c2929867842c90295119df069c9e018
adeebnawshad <adeeb.nawshad@mail.utoronto.ca>
2026-08-04 09:42:48 +0600

    Add: linked-list-cycle-detection - submission-10

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-10.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # length of the cycle is n
+        # fast is x steps ahead of slow
+        # distance = n - x
+        # next iteration fast is (n - x + 1) steps ahead of slow
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
