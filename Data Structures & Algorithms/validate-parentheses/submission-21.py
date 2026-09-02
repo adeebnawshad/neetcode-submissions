6de1402941d0248a41225e4165a9123dac4d7a2d
adeebnawshad <adeeb.nawshad@mail.utoronto.ca>
2026-09-02 10:57:09 +0600

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closeToOpen = {')' : '(', '}' : '{', ']' : '['}
+        stack = []
+        for c in s:
+            if c in closeToOpen:
+                if not stack:
+                    return False
+                if stack.pop() != closeToOpen[c]:
+                    return False
+            else:
+                stack.append(c)
+        if stack:
+            return False
+        return True
